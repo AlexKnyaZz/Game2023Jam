49514f06a3da9291b43af46f3cab3bf1e90a1869
DumbKnight <artemk2803@bk.ru>
2023-12-02 15:11:52 +0300

    Тесты с движением фона

--- Game23/Assets/Scripts/CharacterMovement.cs
+++ Game23/Assets/Scripts/CharacterMovement.cs
@@ -6,19 +6,38 @@ public class CharacterMovement : MonoBehaviour
 {
     public Animator CharacterAnimator;
 
+    public GameObject BackGround;
+
+    public List<GameObject> Planes = new List<GameObject>();
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            Debug.Log(0);
             CharacterAnimator.SetTrigger("Walk");
             CharacterAnimator.ResetTrigger("Idle");
         }
         if (Input.GetKeyUp(KeyCode.W))
         {
-            Debug.Log(1);
             CharacterAnimator.ResetTrigger("Walk");
             CharacterAnimator.SetTrigger("Idle");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 BackGroundPosition = BackGround.transform.position;
+
+            BackGround.transform.position = new Vector3(BackGroundPosition.x - 0.1F, BackGroundPosition.y - 0.1F, BackGroundPosition.z);
+
+            for (int i = 0; i < Planes.Count; i++)
+            {
+                Vector3 BackGroundPosition1 = Planes[i].transform.position;
+
+                Planes[i].transform.position = new Vector3(BackGroundPosition1.x - 0.1F, BackGroundPosition1.y - 0.1F, BackGroundPosition1.z);
+            }
         }
     }
 }
