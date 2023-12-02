653bb35fb773f5657370b260bbff90174b111666
Михаил Новиков <N.Mikhail2002@gmail.com>
2023-12-02 21:43:36 +0300

    menu buttons sounds

---
+++ Game23/Assets/Scripts/ButtonSound.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ButtonSound : MonoBehaviour
+{
+    public AudioSource myFx;
+    public AudioClip hoverFX;
+    public AudioClip clickFX;
+
+    public void HoverSound()
+    {
+        myFx.PlayOneShot(hoverFX);
+    }
+
+    public void ClickSound()
+    {
+        myFx.PlayOneShot(clickFX);  
+    }
+}
